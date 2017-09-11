1f2dfbef9216ab5a529f8a74711c9746d40d4f05
kethan92 <kethan92@gmail.com>
2017-09-11 07:02:20 +0700

    sdfffffffffffffff

---
+++ HotelManagerReponsity_MVC/ViewModel/DateTimeOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelManagerReponsity_MVC.ViewModel
+{
+    public class DateTimeOrder
+    {
+        public DateTime DateTo { get; set; }
+        public DateTime DateFrom { get; set; }
+    }
+}
